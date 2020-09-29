6b6742ba8ed787d311c8955fb778978e7875b7c3
Renaud ANGLES <renaud.angles@asciiparait.onmicrosoft.com>
2020-09-29 16:37:20 +0200

    scaffold tic-tac-toe component

---
+++ src/components/Morpion/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Morpion() {
+    return (
+        <table>
+            <tr>
+                <td>x</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>o</td>
+                <td>o</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>x</td>
+            </tr>
+        </table>
+    )
+}
